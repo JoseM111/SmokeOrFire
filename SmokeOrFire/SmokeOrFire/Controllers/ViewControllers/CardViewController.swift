05924b67fcab9f99d057b8f4436829ae13601069
Junior S <juniorsuarez1014@gmail.com>
2020-05-20 19:21:49 -0400

    made fetching image possible by pressing either buttons

--- SmokeOrFire/SmokeOrFire/Controllers/ViewControllers/CardViewController.swift
+++ SmokeOrFire/SmokeOrFire/Controllers/ViewControllers/CardViewController.swift
@@ -16,19 +16,63 @@ class CardViewController: UIViewController {
     @IBOutlet weak var higherButton: UIButton!
     @IBOutlet weak var lowerButton: UIButton!
     
-    //Mark - LifeCylce
+    //MARK: - LifeCyclce
     override func viewDidLoad() {
         super.viewDidLoad()
+        //TODO - Ian enter some code here
 
     }
     
     //MARK: -Actions
     @IBAction func HigherButtonTapped(_ sender: Any) {
-        
+        CardController.fetchedCard { [weak self] result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let card):
+                    self?.fetchImageAndUpdateView(for: card)
+                case .failure(let error):
+                    self?.presentErrorToUser(localizedError: error)
+                }
+                
+            }
+            
+        }
     }
     
     @IBAction func lowerButtonTapped(_ sender: Any) {
+        CardController.fetchedCard { [weak self] result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let card):
+                    self?.fetchImageAndUpdateView(for: card)
+                case .failure(let error):
+                    self?.presentErrorToUser(localizedError: error)
+                }
+                
+            }
+            
+        }
+    }
+    
+    //MARK: Helper Methods
+    
+    func fetchImageAndUpdateView(for card: Card) {
+        CardController.fetchImage(for: card) { [weak self]
+            result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let image):
+                    self?.gameResultsLabel.text = "Winner Take a Shot"
+                    self?.cardImage.image = image
+                case .failure(let error):
+                    self?.presentErrorToUser(localizedError: error)
+                }
+            }
+        }
     }
     
 
-}
+}// End of Class
